2e4fb3bf19538a417222f5a0560c71d22b504eda
Cody S <cody.scoles@gmail.com>
2022-05-07 23:26:39 -0400

    Add update routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,6 +5,7 @@ const {
   getSingleUser,
   createUser,
   deleteUser,
+  updateUser,
   addFriend,
   removeFriend
 } = require('../../controllers/userController');
@@ -13,7 +14,7 @@ const {
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
 
 // /api/users/:userId/friends/friendId
 router
